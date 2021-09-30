dfe5ec06116c9f49d9b7ed0afd12092b5b9658d7
cd <coolezprogramming@gmail.com>
2021-09-30 17:29:48 -0400

    basic funtionality working

--- script.js
+++ script.js
@@ -8,11 +8,11 @@ function Book(author, title, pages, read, index) {
     this.index = index;
 }
 
-const bookContainer = document.getElementById('bookContainer');
-const addBook = document.getElementById('newBook');
-const popupForm = document.getElementById('form');
-const formButton = document.getElementById('btn');
-const formContainer = document.getElementById('formContainer');
+const bookContainer = document.getElementById("bookContainer");
+const addBook = document.getElementById("newBook");
+const popupForm = document.getElementById("form");
+const formButton = document.getElementById("btn");
+const formContainer = document.getElementById("formContainer");
 
 console.log(formContainer);
 
@@ -25,39 +25,54 @@ function createBook() {
     );
 
     myLibrary.push(book1);
-
+    console.log(myLibrary)
     return (book1.index = myLibrary.indexOf(book1));
 }
 
 function addBookToLibrary() {
-    let bookCard = document.createElement('div');
-    bookCard.classList.add('bookCard');
+    let bookCard = document.createElement("div");
+    bookCard.classList.add("bookCard");
     let index = createBook();
-    let bookValues = document.querySelectorAll('form>input, form>label>input');
+    let bookValues = document.querySelectorAll("form>input, form>label>input");
+    bookCard.id = index;
+
     bookValues.forEach((value) => {
-        let valueDiv = document.createElement('div');
+        let valueDiv = document.createElement("div");
         let valueName = value.name;
-        valueDiv.textContent = myLibrary[index][valueName];
+        if (valueName === "read") {
+            value.checked
+                ? (valueDiv.textContent = "read")
+                : (valueDiv.textContent = "not read");
+        } else {
+            valueDiv.textContent = myLibrary[index][valueName];
+        }
         bookCard.appendChild(valueDiv);
     });
 
-    let removeButton = document.createElement('button');
-    removeButton.textContent = 'remove';
-    bookCard.appendChild(removeButton);
+    let removeButton = document.createElement("button");
+    removeButton.textContent = "remove";
+    removeButton.addEventListener("click", removeBook);
 
+    bookCard.appendChild(removeButton);
     bookContainer.appendChild(bookCard);
 }
 
+function removeBook(index) {
+    bookContainer.removeChild(document.querySelector('.bookCard'))
+    myLibrary.splice(index, 1)
+    console.log(myLibrary)
+}
+
 function resetForm() {
-    popupForm.style.display = 'none';
+    popupForm.style.display = "none";
     addBookToLibrary();
     formContainer.reset();
 }
 
-addBook.addEventListener('click', () => {
-    popupForm.style.display = 'block';
+addBook.addEventListener("click", () => {
+    popupForm.style.display = "block";
 });
 
-formButton.addEventListener('click', () => {
+formButton.addEventListener("click", () => {
     resetForm();
 });

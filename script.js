e9a074e8f3ae8f7bd30216aa541bb7e330536447
ok <coolezprogramming@gmail.com>
2021-09-30 12:27:46 -0400

    added book constructor

--- script.js
+++ script.js
@@ -13,28 +13,33 @@ const popupForm = document.getElementById('form');
 const formButton = document.getElementById('btn');
 const formContainer = document.getElementById('formContainer');
 
-Book.prototype.createBook = () => {
-    console.log(this.title)
+console.log(formContainer)
+
+function createBook() {
+    const book1 = new Book(
+        formContainer.querySelector('[name="author"]').value,
+        formContainer.querySelector('[name="title"]').value,
+        formContainer.querySelector('[name="pages"]').value,
+        formContainer.querySelector('[name="read"]').value
+    );
+    myLibrary.push(book1);
 }
 
 function addBookToLibrary() {
-
     let bookCard = document.createElement('div');
     bookCard.classList.add('bookCard');
-    let author = document.createElement('div')
-    author.id = "author"
-    let title = document.createElement('div')
-    title.id = "title"
-    let pages = document.createElement('div')
-    pages.id = "pages"
-    let read = document.createElement('div')
-    read.id = "read"
-
+    let author = document.createElement('div');
+    author.id = 'author';
+    let title = document.createElement('div');
+    title.id = 'title';
+    let pages = document.createElement('div');
+    pages.id = 'pages';
+    let read = document.createElement('div');
+    read.id = 'read';
 
     bookContainer.appendChild(bookCard);
-    bookCard.append(author, title, pages, read)
+    bookCard.append(author, title, pages, read);
 }
-
 
 function resetForm() {
     popupForm.style.display = 'none';
@@ -46,5 +51,6 @@ addBook.addEventListener('click', () => {
     popupForm.style.display = 'block';
 });
 
-
-formButton.addEventListener('click', resetForm);
+formButton.addEventListener('click', () => {
+    createBook(), resetForm()
+});
